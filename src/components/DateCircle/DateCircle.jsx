22b5b176bcea096e81e68111dfd8e37c0ed4a85d
RenataGaraeva <renatagaraeva12@gmail.com>
2025-09-22 19:33:10 +0300

    feat/create component date in circle

---
+++ src/components/DateCircle/DateCircle.jsx
@@ -0,0 +1,25 @@
+import './dateCircle.scss'
+import {useContext} from "react";
+import {PropsContext} from "../../context/context.jsx";
+
+export default function DateCircle () {
+    const {
+        currentDates,
+        endDateRef,
+        startDateRef,
+} = useContext(PropsContext);
+
+    return (
+        <div className='dates'>
+            <div >
+        <span ref={startDateRef} className='startDate'>
+          {currentDates[0]}
+        </span>
+                {' '}
+                <span ref={endDateRef} className='finishDate'>
+          {currentDates[1]}
+        </span>
+            </div>
+        </div>
+    );
+}
