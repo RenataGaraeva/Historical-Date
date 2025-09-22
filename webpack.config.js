2585250df8ab7102d5ee38fef8ffbfac842b1861
RenataGaraeva <renatagaraeva12@gmail.com>
2025-09-22 23:37:52 +0300

    refactor/change config

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,7 @@ module.exports = {
     static: {
       directory: path.join(__dirname, 'dist'), // Каталог для статики
     },
+    historyApiFallback: true, //добавила сама
     open: true, // Автоматически открывать браузер
   },
 
